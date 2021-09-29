382f7161a784f8b075fab4935ec4a329885edb19
James Trimarco <james.trimarco@gmail.com>
2021-09-29 08:08:53 -0400

    code from YouTube tutorial

--- ner_client.py
+++ ner_client.py
@@ -1,8 +1,15 @@
-import spacy
-
 class NamedEntityClient:
     def __init__(self, model):
         self.model = model
 
     def get_ents(self, sentence) -> dict:
-        return {}
+        doc = self.model(sentence)
+        entities = [
+            {"ent": ent.text, "label": self.map_label(ent.label_)} for ent in doc.ents
+        ]
+        return {"ents": entities, "html": ""}
+
+    @staticmethod
+    def map_label(label):
+        label_map = {"PERSON": "Person", "NORP": "Group", "LOC": "Location"}
+        return label_map.get(label)
--- test_doubles.py
+++ test_doubles.py
@@ -2,6 +2,7 @@ class NerModelTestDouble:
     """
     Test double for spaCy NLP model
     """
+
     def __init__(self, model) -> None:
         self.model = model
 
@@ -11,9 +12,27 @@ class NerModelTestDouble:
     def __call__(self, sent):
         return DocTestDouble(sent, self.ents)
 
+
 class DocTestDouble:
     """
-    This is a test double for spaCy Doc.  
+    This is a test double for spaCy Doc.
     """
+
     def __init__(self, sent, ents) -> None:
-        self.ents = [SpanTestDouble(ent['text'], ent['label'] for ent in ents)]
+        self.ents = [SpanTestDouble(ent["text"], ent["label_"]) for ent in ents]
+
+    # def patch_method(self, attr, return_value):
+    #     """
+    #     Allows patching of methods from real spaCy
+    #     """
+    #     def patched(): return return_value
+    #     setattr(self, attr, patched)
+    #     return self
+
+class SpanTestDouble:
+    """
+    Test double for spaCy Span
+    """
+    def __init__(self, text, label) -> None:
+        self.text = text
+        self.label_ = label
