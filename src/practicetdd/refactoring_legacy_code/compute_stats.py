ab272bb7d351e98f8591266056152711b161e104
James Trimarco <james.trimarco@gmail.com>
2021-10-05 08:26:04 -0400

    code and data

--- src/practicetdd/refactoring_legacy_code/compute_stats.py
+++ src/practicetdd/refactoring_legacy_code/compute_stats.py
@@ -1,3 +1,29 @@
+from typing import List
+
+# Re-factor the function compute_stats into the following functions:
+# read_ints: Reads in the data from random_nums.txt and convert them into a list of integers
+# count: Returns the total number of elements in random_nums.txt
+# summation: Returns the sum of all of the elements in random_nums.txt
+# average: Returns the average of all of the elements in random_nums.txt
+# minimum: Returns the smallest integer in random_nums.txt
+# maximum: Returns the largest integer in random_nums.txt
+
+
+def read_ints(file_name: str) -> List[int]:
+    with open(file_name, "r") as file:
+        ints = []
+        for line in file:
+            if line.strip().isnumeric():
+                ints.append(int(line))
+            else:
+                continue
+    return ints
+
+
+def count(data: List[int]) -> int:
+    return len(data)
+
+
 def compute_stats(file):
     total = 0
     sum = 0
@@ -24,4 +50,4 @@ def compute_stats(file):
 
 
 if __name__ == "__main__":
-    compute_stats("random_nums.txt")
+    read_ints("random_nums.txt")
