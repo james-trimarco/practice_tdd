c159d41198fc2d96c2d6d2981e6afc6431e637d6
James Trimarco <james.trimarco@gmail.com>
2021-10-12 08:42:41 -0400

    Commenting out NER tests
    These are from a different project and aren't under active development

--- test/test_ner_client.py
+++ test/test_ner_client.py
@@ -1,78 +1,78 @@
-import unittest
-from practicetdd.NER.ner_client import NamedEntityClient
-from practicetdd.NER.test_doubles import NerModelTestDouble
+# import unittest
+# from practicetdd.NER.ner_client import NamedEntityClient
+# from practicetdd.NER.test_doubles import NerModelTestDouble
 
 
-class TestNerClient(unittest.TestCase):
-    def test_get_ents_returns_dictionary_given_empty_string_causes_empty_spacy_doc_ents(
-        self,
-    ):
-        model = NerModelTestDouble("eng")
-        model.returns_doc_ents([])
-        ner = NamedEntityClient(model)
-        ents = ner.get_ents("")
-        self.assertIsInstance(ents, dict)
+# class TestNerClient(unittest.TestCase):
+#     def test_get_ents_returns_dictionary_given_empty_string_causes_empty_spacy_doc_ents(
+#         self,
+#     ):
+#         model = NerModelTestDouble("eng")
+#         model.returns_doc_ents([])
+#         ner = NamedEntityClient(model)
+#         ents = ner.get_ents("")
+#         self.assertIsInstance(ents, dict)
 
-    def test_get_ents_returns_list_given_nonempty_input_causes_empty_spacy_doc_ents(
-        self,
-    ):
-        model = NerModelTestDouble("eng")
-        # assumes we don't get back any doc ents
-        model.returns_doc_ents([])
-        ner = NamedEntityClient(model)
-        ents = ner.get_ents("Madison is a city in Wisconsin")
-        self.assertIsInstance(ents, dict)
+#     def test_get_ents_returns_list_given_nonempty_input_causes_empty_spacy_doc_ents(
+#         self,
+#     ):
+#         model = NerModelTestDouble("eng")
+#         # assumes we don't get back any doc ents
+#         model.returns_doc_ents([])
+#         ner = NamedEntityClient(model)
+#         ents = ner.get_ents("Madison is a city in Wisconsin")
+#         self.assertIsInstance(ents, dict)
 
-    def test_get_ents_given_spacy_PERSON_is_returned_serializes_to_Person(self):
-        model = NerModelTestDouble("eng")
-        doc_ents = [{"text": "Richie Floyd", "label_": "PERSON"}]
-        model.returns_doc_ents(doc_ents)
-        ner = NamedEntityClient(model)
-        result = ner.get_ents("...")
-        expected_result = {
-            "ents": [{"ent": "Richie Floyd", "label": "Person"}],
-            "html": "",
-        }
-        self.assertListEqual(result["ents"], expected_result["ents"])
+#     def test_get_ents_given_spacy_PERSON_is_returned_serializes_to_Person(self):
+#         model = NerModelTestDouble("eng")
+#         doc_ents = [{"text": "Richie Floyd", "label_": "PERSON"}]
+#         model.returns_doc_ents(doc_ents)
+#         ner = NamedEntityClient(model)
+#         result = ner.get_ents("...")
+#         expected_result = {
+#             "ents": [{"ent": "Richie Floyd", "label": "Person"}],
+#             "html": "",
+#         }
+#         self.assertListEqual(result["ents"], expected_result["ents"])
 
-    def test_get_ents_given_spacy_NORP_is_returned_serializes_to_Group(self):
-        model = NerModelTestDouble("eng")
-        doc_ents = [{"text": "Albanian", "label_": "NORP"}]
-        model.returns_doc_ents(doc_ents)
-        ner = NamedEntityClient(model)
-        result = ner.get_ents("...")
-        expected_result = {
-            "ents": [{"ent": "Albanian", "label": "Group"}],
-            "html": "",
-        }
-        self.assertListEqual(result["ents"], expected_result["ents"])
+#     def test_get_ents_given_spacy_NORP_is_returned_serializes_to_Group(self):
+#         model = NerModelTestDouble("eng")
+#         doc_ents = [{"text": "Albanian", "label_": "NORP"}]
+#         model.returns_doc_ents(doc_ents)
+#         ner = NamedEntityClient(model)
+#         result = ner.get_ents("...")
+#         expected_result = {
+#             "ents": [{"ent": "Albanian", "label": "Group"}],
+#             "html": "",
+#         }
+#         self.assertListEqual(result["ents"], expected_result["ents"])
 
-    def test_get_ents_given_spacy_LOC_is_returned_serializes_to_Location(self):
-        model = NerModelTestDouble("eng")
-        doc_ents = [{"text": "Tirana", "label_": "LOC"}]
-        model.returns_doc_ents(doc_ents)
-        ner = NamedEntityClient(model)
-        result = ner.get_ents("...")
-        expected_result = {
-            "ents": [{"ent": "Tirana", "label": "Location"}],
-            "html": "",
-        }
-        self.assertListEqual(result["ents"], expected_result["ents"])
+#     def test_get_ents_given_spacy_LOC_is_returned_serializes_to_Location(self):
+#         model = NerModelTestDouble("eng")
+#         doc_ents = [{"text": "Tirana", "label_": "LOC"}]
+#         model.returns_doc_ents(doc_ents)
+#         ner = NamedEntityClient(model)
+#         result = ner.get_ents("...")
+#         expected_result = {
+#             "ents": [{"ent": "Tirana", "label": "Location"}],
+#             "html": "",
+#         }
+#         self.assertListEqual(result["ents"], expected_result["ents"])
 
-    def test_get_ents_given_multiple_ents_serializes_all(self):
-        model = NerModelTestDouble("eng")
-        doc_ents = [
-            {"text": "Tirana", "label_": "LOC"},
-            {"text": "Judith Butler", "label_": "PERSON"},
-        ]
-        model.returns_doc_ents(doc_ents)
-        ner = NamedEntityClient(model)
-        result = ner.get_ents("...")
-        expected_result = {
-            "ents": [
-                {"ent": "Tirana", "label": "Location"},
-                {"ent": "Judith Butler", "label": "Person"},
-            ],
-            "html": "",
-        }
-        self.assertListEqual(result["ents"], expected_result["ents"])
+#     def test_get_ents_given_multiple_ents_serializes_all(self):
+#         model = NerModelTestDouble("eng")
+#         doc_ents = [
+#             {"text": "Tirana", "label_": "LOC"},
+#             {"text": "Judith Butler", "label_": "PERSON"},
+#         ]
+#         model.returns_doc_ents(doc_ents)
+#         ner = NamedEntityClient(model)
+#         result = ner.get_ents("...")
+#         expected_result = {
+#             "ents": [
+#                 {"ent": "Tirana", "label": "Location"},
+#                 {"ent": "Judith Butler", "label": "Person"},
+#             ],
+#             "html": "",
+#         }
+#         self.assertListEqual(result["ents"], expected_result["ents"])
