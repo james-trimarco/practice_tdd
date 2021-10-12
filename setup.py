ba4845973d9a427c04ffabf01613e83a3aecfcad
James Trimarco <james.trimarco@gmail.com>
2021-10-12 08:38:07 -0400

    Revert "Start coursera exercises"

--- setup.py
+++ setup.py
@@ -1,17 +1,8 @@
 from distutils.core import setup
 from setuptools import find_packages
-from os.path import basename, splitext
-from glob import glob
 
 setup(
-    name="practicetdd",
+    name="practice_tdd",
     version="0.0.1",
     description="A place for James to practice TDD",
-    author="James Trimarco",
-    author_email="james.trimarco@gmail.com",
-    install_requires=[],
-    include_package_data=True,
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
 )
--- test/test_ner_client.py
+++ test/test_ner_client.py
@@ -1,6 +1,4 @@
 import unittest
-from practicetdd.NER.ner_client import NamedEntityClient
-from practicetdd.NER.test_doubles import NerModelTestDouble
 
 
 class TestNerClient(unittest.TestCase):
